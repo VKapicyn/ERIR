61d96efa2d173b30009d606567c7955e826085c2
VKapicyn <zveronym@gmail.com>
2016-11-25 19:13:26 +0300

    post.createCompany

--- app/api.js
+++ app/api.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 var db = mongoose.connect("mongodb://localhost:27017/ERIO")//конектимсся к БД
 
+var multer = require("multer");
+var upload = multer({dest: './src/buffer'});
+var conn = mongoose.connection;
+var fs = require('fs');
+var Grid = require('gridfs-stream');
+Grid.mongo = mongoose.mongo;
+gfs=Grid(conn.db);
 
 //схемы данных
 var Report = require('./models/reportModel').ReportModel; // модель отчета
@@ -13,15 +20,45 @@ exports.addReport = function(req,res){
 }
 
 exports.addCompany = function(req,res){
-    //var name="name "+req.params.id;
-    var testcompany = new Company;//({name:"ttt"});
-    testcompany.name="777";
-    testcompany.save();
-    //testcompany.name="name "+req.params.id;
-    //testcompany.create({name}, function (err) {
-    //if (err) return handleError(err)});
-    res.render('report',{ report: 'Отчет №' + 777, id: "Работает"});
+    var new_comp = new Company;
+    //new_comp.id = new mongoose.Types.ObjectId;
+    new_comp.name = req.body.name;
+
+    //cохраняем лого в БД
+    req.files.logo.mv('src/buffer/'+new_comp._id+req.files.logo.name, function(err){});
+
+    var writestream = gfs.createWriteStream({
+      filename: new_comp._id+req.files.logo.name
+    });
+
+    fs.createReadStream('src/buffer/'+new_comp._id+req.files.logo.name)
+      .on('end', function(){fs.unlink('src/buffer/'+new_comp._id+req.files.logo.name, function(err){console.log("success")})})
+        .on('err', function(){ console.log('Error uploading image')})
+          .pipe(writestream);
+ 
+    writestream.on('close', function (file) {
+        console.log(file.filename + ' Written To DB');
+    });
+    new_comp.logo = '/'+new_comp._id+req.files.logo.name;
+
+    new_comp.info = req.body.info;
+    new_comp.adress = req.body.adress; 
+    new_comp.telephone = req.body.telephone;
+    new_comp.CEO = req.body.CEO;
+    new_comp.fax = req.body.fax; 
+    new_comp.link = req.body.link;
+    new_comp.consult = req.body.consult;
+    new_comp.save();
+    res.send('ok '+new_comp._id+' ');
 }
+
+exports.getFile = function(req, res){
+      var readstream = gfs.createReadStream({filename: req.params.filename});
+      readstream.on('error', function(err){
+        res.send('No image found with that title');
+      });
+      readstream.pipe(res);
+  };
 
 exports.getReportById = function(req, res){
     /*
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');// берём Express
 var control = require('./app/controllers/pages'); //Общий контроллер страниц
 var api = require('./app/api'); //api для компаний и отчетов
 var app = express(); // создаём Express-приложение
+var bodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
 
+
+app.use(fileUpload());
+app.use(bodyParser());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/src'));//подключаем статику(изображения, стили, клиентский js)
 app.set('views', './app/views'); //указыываем папку с вьюшками
 app.set('view engine', 'jade'); // 'view engine' - механизм визуализации данного шаблонизатора
@@ -18,9 +24,12 @@ app.get('/admin', control.AdminPage); // админка
 //маршрутизация API запросов
 app.get('/report/:id', api.getReportById); // выводим отчет по id
 app.get('/company/:id', api.getCompanyById); // выводим компанию по id
-app.get('/company/create/:id', api.addCompany); //создаем компанию по id HALT! передалать
+app.post('/company/create/', api.addCompany); //создаем компанию
 app.get('/sector/:name', api.getSectorByName); //ищем отрасль по имени
 app.get('/companies/sector/:name',api.getCompaniesBySectorName);//ищем компании по названию отрасли 
+
+app.get('/:filename', api.getFile);//возвращаем файл из БД
+
 // запускаем сервер на порту 8080
 app.listen(8080);
 console.log('Server started!');
