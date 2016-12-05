3ca16cecaf278bfd852b24b0a1b02098caf79977
IvanBlacky <tcherivan@gmail.com>
2016-12-05 15:56:36 +0300

    Поправки

--- app/models/companyModel.js
+++ app/models/companyModel.js
@@ -3,46 +3,41 @@ var mongoose = require('mongoose');
 //Схема данных компании. 
 var CompanySchema = new mongoose.Schema({
     //уникальный идентификатор
-    _id : mongoose.Schema.Types.ObjectId, 
-    accept : Boolean,
+    _id: String,
+    accept: Boolean,
     date: {
         type: mongoose.Schema.Types.Date,
-        default:  mongoose.Schema.Types.Date.now
+        default: mongoose.Schema.Types.Date.now
     },
-    logo : String,
-    name : String, 
-    info : String, 
+    logo: String,
+    name: String,
+    info: String,
     short_name: String,
     opf: String,
-    telephone : String,
-    sector : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'sector'
-    }, 
-    adress : String, //?
+    telephone: String,
+    sector: {
+        type: String,
+    },
+    adress: String, //?
     size_of_company: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'size_of_company'
+        type: String,
     },
     type_of_ownership: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'type_of_ownership'
+        type: String,
     },
     employees: Number,
     revenue: Number,
     //listing????
     listing: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'listing'
+        type: String,
     },
-    fax : String, 
-    CEO : String, 
-    link : String, 
-    email : String,
+    fax: String,
+    CEO: String,
+    link: String,
+    email: String,
     reports: [{
-        report : {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'report'
+        report: {
+            type: String,
         }
     }]
 })
