593f04a3a3ede4c6b5c57d054b33e2b145d98ea3
IvanBlacky <tcherivan@gmail.com>
2016-12-07 10:06:24 +0300

    Сделана модель отчета

--- app/models/reportModel.js
+++ app/models/reportModel.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
-//Схема данных отчета. 
-//Доделать!!!
+//Схема данных отчета.
 var ReportSchema = new mongoose.Schema({
     //уникальный идентификатор
     _id: mongoose.Schema.Types.ObjectId,
@@ -13,11 +12,34 @@ var ReportSchema = new mongoose.Schema({
         default: mongoose.Schema.Types.Date.now
     },
     name: String,
+    type: String, //социальный, годовой, об устойчивом развитии и т.п.
     year: Number,
+    standarts: [{
+        G4:{
+        type: Boolean
+        },
+        G3:{
+        type: Boolean
+        },
+        IIRC:{
+            type: Boolean
+        } //и прочие страндарты сюда
+    }],
+    sector: String,
+    HQAdress: String,
+    Size: String,
+    OPF: String,
+    TypeOfProperty: String,
+    BestPractise: Boolean, //является лучшей практикой?
+    BPDescription: String, //если предыдущее да, то описание
     preview: String,
+    ratingPos: String, //пишем место, либо – "Не листингуется"
     company: {
         type: String
     },
+    BusinessModel: Boolean,//
+    Strategy: Boolean,     //Есть ли элемент в отчете
+    RiskMap: Boolean,      //
     interactive: String,
     doc_rus: String, 
     doc_en: String,
