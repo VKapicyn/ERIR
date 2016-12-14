1a95466c17d41730221debcb085023046f9b6107
VKapicyn <zveronym@gmail.com>
2016-12-14 20:21:52 +0300

    поиск по компаниям

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -17,15 +17,41 @@ var size_of_company;
 var type_of_ownership; 
 var company;
 var reports;
+var sort = req.params.sort;
 
-    Company.find({}).then(function (_company){
+//переделать индусский код через query
+    if (sort == undefined)
+    //сделать по дате добавления
+    Company.find({}).sort({}).exec(function (err, _company){
         company = _company;
-            Static.find({}, function (err, docs){
+        rend();
+    });
+
+    if (sort == 'name')
+    Company.find({}).sort({name : 'asc'}).exec(function (err, _company){
+        company = _company;
+        rend();
+    });
+
+    if (sort == 'sector')
+    Company.find({}).sort({sector : 'asc'}).exec(function (err, _company){
+        company = _company;
+        rend();
+    });
+
+    if (sort == 'size_of_company')
+    Company.find({}).sort({size_of_company : 'asc'}).exec(function (err, _company){
+        company = _company;
+        rend();
+    });
+
+    function rend(){
+        Static.find({}, function (err, docs){
             var i;
 
             for(i=0; i<docs.length-1; i++){
                 switch(docs[i].name){
-                    case 'sector': {sector = docs[i].mass; console.log(sector);}
+                    case 'sector': sector = docs[i].mass;
                         break;
                     case 'size_of_company': size_of_company = docs[i].mass;
                         break;
@@ -35,5 +61,5 @@ var reports;
 
             res.render('search-company', {sector:sector, size_of_company:size_of_company, company:company});
         });
-    });
+    }; 
 };
