c4197709f3e6b01e466e6fa260ac3c617b026f36
VKapicyn <zveronym@gmail.com>
2016-12-14 19:32:56 +0300

    правки в разделе поиска

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var Company = require('../models/companyModel').companyModel;
 var Report = require('../models/reportModel').reportModel;
+var Static = require('../models/staticModel').staticModel;
 
 exports.searchReportPage = function (req, res){
     Report.find({}).then(function (reports){
@@ -10,9 +11,29 @@ exports.searchReportPage = function (req, res){
 };
 
 exports.searchCompanyPage = function (req, res){
-    Company.find({}).then(function (company){
-        Report.find({}).then(function (reports){
-            res.render('search-company', {company:company, reports:reports});
+var sector;
+var opf;
+var size_of_company;
+var type_of_ownership; 
+var company;
+var reports;
+
+    Company.find({}).then(function (_company){
+        company = _company;
+            Static.find({}, function (err, docs){
+            var i;
+
+            for(i=0; i<docs.length-1; i++){
+                switch(docs[i].name){
+                    case 'sector': {sector = docs[i].mass; console.log(sector);}
+                        break;
+                    case 'size_of_company': size_of_company = docs[i].mass;
+                        break;
+                    default : continue;
+                }
+            }
+
+            res.render('search-company', {sector:sector, size_of_company:size_of_company, company:company});
         });
     });
 };
